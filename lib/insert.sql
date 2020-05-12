9dcf5c9271e33e4c63546ea77f14e038e6575806
ReginaF2012 <skritschy@gmail.com>
2020-05-12 12:58:21 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,66 @@
+INSERT INTO users (name, age) VALUES ('Frank', 20);
+INSERT INTO users (name, age) VALUES ('Amanda', 35);
+INSERT INTO users (name, age) VALUES ('Charlotte', 22);
+INSERT INTO users (name, age) VALUES ('Bob', 53);
+INSERT INTO users (name, age) VALUES ('Tod', 40);
+INSERT INTO users (name, age) VALUES ('Sam', 18);
+INSERT INTO users (name, age) VALUES ('Carol', 38);
+INSERT INTO users (name, age) VALUES ('Hannah', 29);
+INSERT INTO users (name, age) VALUES ('Arin', 33);
+INSERT INTO users (name, age) VALUES ('Marcus', 26);
+INSERT INTO users (name, age) VALUES ('Sarah', 33);
+INSERT INTO users (name, age) VALUES ('Dylan', 33);
+INSERT INTO users (name, age) VALUES ('Rick', 58);
+INSERT INTO users (name, age) VALUES ('Mark', 69);
+INSERT INTO users (name, age) VALUES ('Jack', 88);
+INSERT INTO users (name, age) VALUES ('Melvin', 27);
+INSERT INTO users (name, age) VALUES ('Francine', 35);
+INSERT INTO users (name, age) VALUES ('Stanley', 35);
+INSERT INTO users (name, age) VALUES ('Mathew', 29);
+INSERT INTO users (name, age) VALUES ('Billy', 41);
+
+
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("EOS", "Blockchain", 25000000 ,"05/12/2008", "06/1/2018");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Star Citizen", "Video game", 2000000, "06/19/2018", "Ongoing");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Filecoin", "Blockchain", 1000000, "08/28/2015", "09/7/2017");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Tezos", "Blockchain", 200000000, "05/26/2012", "07/14/2017");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Sirin Labs (SRN)", "Mobile", 150000000, "12/1/2016", "12/1/2017");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Bancor protocol", "Blockchain", 100000000, "06/1/2015", "06/12/2017");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("The DAO", "Blockchain", 500000, "01/1/2014", "05/28/2016");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Polkadot", "Blockchain", 100000000, "08/15/2016", "10/27/2017");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Sono Motors Sion", "Solar Electric Vehicle", 50000000 ,"01/15/2018", "01/20/2020");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("CyberMiles", "Ecommerce", 30000000, "08/10/2010", "11/22/2017");
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 100, 1, 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 20, 1, 2);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 25, 2, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 1000, 1, 4);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 5, 5, 5);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 10, 6, 6);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 3, 18, 7);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 25, 20, 8);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 10, 10, 9);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 50, 8, 10);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 10, 9, 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 5, 13, 2);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 15, 14, 2);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 20, 15, 5);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 25, 16, 5);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 200, 17, 10);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 15, 3, 8);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 50, 4, 8);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 100, 5, 7);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 30, 6, 6);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 25, 7, 5);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 5, 8, 4);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 10, 9, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 20, 10, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 25, 11, 2);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 30, 12, 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 50, 13, 8);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 5, 14, 9);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 5, 16, 7);
+INSERT INTO pledges (amount, user_id, project_id) VALUES ( 10, 15, 6);
+
+
